b99fa93208302cc68edd8269e82a565e45014a04
schnsrw <140796390+schnsrw@users.noreply.github.com>
2025-07-27 01:27:47 +0530

    feat(storage): add entity service and controller improvements

--- src/main/java/in/lazygod/repositories/StorageRepository.java
+++ src/main/java/in/lazygod/repositories/StorageRepository.java
@@ -1,7 +1,10 @@
 package in.lazygod.repositories;
 
 import in.lazygod.models.Storage;
+import in.lazygod.models.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StorageRepository extends JpaRepository<Storage, String> {
+    List<Storage> findByOwner(User owner);
 }
