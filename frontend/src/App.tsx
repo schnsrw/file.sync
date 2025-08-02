0cf60c795a074831f6417ab9baf7d9fde52bd511
sachin sarwa <schnsrw@gmail.com>
2025-08-02 21:20:36 +0530

    removed frontend directory

--- frontend/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import AppRouter from './routes/AppRouter';
-import './index.css';
-
-function App() {
-  return <AppRouter />;
-}
-
-export default App;
--- frontend/src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from '../components/DashboardLayout';
-
-export default function Dashboard() {
-  return (
-    <DashboardLayout>
-      <p className="text-gray-700">Dashboard</p>
-    </DashboardLayout>
-  );
-}
